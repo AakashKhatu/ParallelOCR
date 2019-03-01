5acd6ba344d9e8d5eb0b68e6f4673f3452df4be6
zer0k <aakashkhatu1998@gmail.com>
2019-03-01 16:54:50 +0530

    fixed gitignore & added cropping of lines

--- separate.py
+++ separate.py
@@ -3,6 +3,7 @@ import numpy as np
 from statistics import mean
 
 filename = '1'
+threshold = 255
 
 start=0
 f=True
@@ -11,7 +12,7 @@ part = []
 img = cv2.imread('processed_files/{0}.png'.format(filename), cv2.IMREAD_GRAYSCALE)
 height, width = img.shape[:2]
 for i, row in enumerate(img):
-    if mean(row) == 255:
+    if mean(row) >= threshold:
         if f is False:
             f = True
             start = i
@@ -21,9 +22,8 @@ for i, row in enumerate(img):
             mid = int((start+i)/2)
             part.append(mid)
             cv2.line(img, (0, mid), (width, mid), 0, 1)
-cv2.imshow("blank lines", img)
+cv2.imwrite("processed_files/{0}_lines.png".format(filename), img)
 cv2.waitKey(0)
-print(part)
 
-for s, e in zip(part, part[1:]):
-    print((s, e))
+for i, (s, e) in enumerate(zip(part, part[1:])):
+    cv2.imwrite("sentences/{0}_{1}.png".format(filename, i), img[s:e])
